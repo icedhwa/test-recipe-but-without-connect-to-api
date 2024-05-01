961e12e0689bdffca24d8fd00febf0b7cc36c58e
icedhwa <162160689+icedhwa@users.noreply.github.com>
2024-05-01 22:48:31 +0300

    Add files via upload

---
+++ src/app/landing-image/mappings.ts
@@ -0,0 +1,272 @@
+export const IMG_MAP = {
+    "food1": {
+        "author": {
+            "link": "https://visualhunt.co/a7/5fe707b9",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/cb32c7c4"
+        }
+    },
+    "food2": {
+        "author": {
+            "link": "https://visualhunt.co/a7/db767ba5",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food3": {
+        "author": {
+            "link": "https://visualhunt.co/a7/4be16335",
+            "name": "ella.o"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food4": {
+        "author": {
+            "link": "https://visualhunt.co/a7/47bd101b",
+            "name": "ella.o"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food5": {
+        "author": {
+            "link": "https://visualhunt.co/a7/18598fea",
+            "name": "timsackton"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food6": {
+        "author": {
+            "link": "https://visualhunt.co/a7/ca46d2d6",
+            "name": "ella.o"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food7": {
+        "author": {
+            "link": "https://visualhunt.co/a7/e7c37751",
+            "name": "Thank You (24 Millions ) views"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food8": {
+        "author": {
+            "link": "https://visualhunt.co/a7/72b23cd8",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food9": {
+        "author": {
+            "link": "https://visualhunt.co/a7/a0d15fe9",
+            "name": "spurekar"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food10": {
+        "author": {
+            "link": "https://visualhunt.co/a7/6be29a9d",
+            "name": "ella.o"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food11": {
+        "author": {
+            "link": "https://visualhunt.co/a7/d95e8677",
+            "name": "ella.o"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food12": {
+        "author": {
+            "link": "https://visualhunt.co/a7/00c9a847",
+            "name": "SodanieChea"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food13": {
+        "author": {
+            "link": "https://visualhunt.co/a7/15b1822e",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food14": {
+        "author": {
+            "link": "https://visualhunt.co/a7/78befff5",
+            "name": "Jonas Weckschmied"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food15": {
+        "author": {
+            "link": "https://visualhunt.co/a7/cec7f200",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food16": {
+        "author": {
+            "link": "https://visualhunt.co/a7/97b689d3",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food17": {
+        "author": {
+            "link": "https://visualhunt.co/a7/aacc9c2c",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food18": {
+        "author": {
+            "link": "https://visualhunt.co/a7/195d6243",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food19": {
+        "author": {
+            "link": "https://visualhunt.co/a7/4c3b0fe4",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food20": {
+        "author": {
+            "link": "https://visualhunt.co/a7/01af57ab",
+            "name": "eye-shuh"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food21": {
+        "author": {
+            "link": "https://visualhunt.co/a7/af2570b2",
+            "name": "Park City Area Restaurant"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food22": {
+        "author": {
+            "link": "https://visualhunt.co/a7/686de12c",
+            "name": "FitTasteTic"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food23": {
+        "author": {
+            "link": "https://visualhunt.co/a7/21cbc0b0",
+            "name": "homethods"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food24": {
+        "author": {
+            "link": "https://visualhunt.co/a7/30fc7b6a",
+            "name": "ginnerobot"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food25": {
+        "author": {
+            "link": "https://visualhunt.co/a7/53997d3f",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food26": {
+        "author": {
+            "link": "https://visualhunt.co/a7/4caa64d1",
+            "name": "Red Moon Sanctuary"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food27": {
+        "author": {
+            "link": "https://visualhunt.co/a7/028efd04",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food28": {
+        "author": {
+            "link": "https://visualhunt.co/a7/f461b44b",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food29": {
+        "author": {
+            "link": "https://visualhunt.co/a7/bc7ca1a8",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    },
+    "food30": {
+        "author": {
+            "link": "https://visualhunt.co/a7/e32ab5d7",
+            "name": "wuestenigel"
+        },
+        "site": {
+            "link": "https://visualhunt.com/re10/56a686eb"
+        }
+    }
+}
